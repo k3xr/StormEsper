613332b023207d50c812156867194f2976dfc61f
oscarf <k3ther@gmail.com>
2018-05-01 10:54:05 +0200

    Added Topology cluster 3

---
+++ src/main/java/master/storm/esper/example/TopologyCluster3.java
@@ -0,0 +1,30 @@
+package master.storm.esper.example;
+
+import org.apache.storm.Config;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
+import org.apache.storm.topology.TopologyBuilder;
+
+public class TopologyCluster3 {
+
+	
+	public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException, AuthorizationException {
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("tempSpout", new TemperatureSpout());
+		builder.setBolt("filterBolt", new FilterBolt(TemperatureSpout.TEMPERATURE_FIELDNAME, 100, "A1"),4)
+			.shuffleGrouping("tempSpout",TemperatureSpout.TEMPERATURE_STREAMNAME);
+		builder.setBolt("filterBoltA2", new FilterBolt(TemperatureSpout.TEMPERATURE_FIELDNAME, 70, "A2")).setNumTasks(8)
+			.shuffleGrouping("tempSpout",TemperatureSpout.TEMPERATURE_STREAMNAME);
+		
+		
+
+		Config config = new Config();
+		config.setNumWorkers(Integer.parseInt(args[0]));
+		StormSubmitter.submitTopology("TemperatureTopology3", config, builder.createTopology());
+		
+		
+		
+	}
+}
